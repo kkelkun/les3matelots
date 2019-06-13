9e0ebe6fcc105493d14c73ebca9f1f76dc98e022
thibault <t.capelli@wanadoo.fr>
2019-06-13 05:02:15 +0200

    fix loading styled-components

--- src/components/Parallax/Parallax.js
+++ src/components/Parallax/Parallax.js
@@ -5,14 +5,18 @@ import PropTypes from "prop-types"
 class Parallax extends React.Component {
   constructor(props) {
     super(props)
-    var windowScrollTop = window.pageYOffset / 3 || 0
+    if (typeof window !== `undefined`) {
+      var windowScrollTop = window.pageYOffset / 3
+    }
     this.state = {
-      transform: "translate3d(0," + windowScrollTop() + "px,0)",
+      transform: "translate3d(0," + windowScrollTop + "px,0)",
     }
     this.resetTransform = this.resetTransform.bind(this)
   }
   componentDidMount() {
-    var windowScrollTop = window.pageYOffset / 3 || 0
+    if (typeof window !== `undefined`) {
+      var windowScrollTop = window.pageYOffset / 3
+    }
     this.setState({
       transform: "translate3d(0," + windowScrollTop() + "px,0)",
     })
@@ -22,7 +26,9 @@ class Parallax extends React.Component {
     window.removeEventListener("scroll", this.resetTransform)
   }
   resetTransform() {
-    var windowScrollTop = window.pageYOffset / 3 || 0
+    if (typeof window !== `undefined`) {
+      var windowScrollTop = window.pageYOffset / 3
+    }
     this.setState({
       transform: "translate3d(0," + windowScrollTop() + "px,0)",
     })
